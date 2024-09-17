cc99e651bbd735209a93a3bff316d125d8d916df
deanitasekar <deanitasekar@gmail.com>
2024-09-17 13:41:59 +0700

    add form input data

--- main/views.py
+++ main/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.forms import ProductEntryForm
+from main.models import Product
 
 def show_main(request):
+    products = Product.objects.all()
     context = {
         'npm' : '2306229405',
         'name': 'Deanita Sekar Kinasih',
-        'class': 'PBP D'
+        'class': 'PBP D',
+        'products' : products,
     }
 
     return render(request, "main.html", context)
+
+def create_product(request):
+    form = ProductEntryForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_product.html", context)
