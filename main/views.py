3a19ff0312653cc82f336e794c56e14432a7f8fe
deanitasekar <deanitasekar@gmail.com>
2024-10-01 04:19:20 +0700

    fitur delete product

--- main/views.py
+++ main/views.py
@@ -99,5 +99,15 @@ def edit_product (request, id):
         return HttpResponseRedirect(reverse('main:show_main'))
     
     context = {'form': form}
-    return render(request, "edit_mood.html", context)
+    return render(request, "edit_product.html", context)
+
+def delete_product(request, id):
+    # Get product berdasarkan id
+    product = Product.objects.get(pk = id)
+    
+    # Hapus product
+    product.delete()
+    
+    # Kembali ke halaman awal
+    return HttpResponseRedirect(reverse('main:show_main'))
     
