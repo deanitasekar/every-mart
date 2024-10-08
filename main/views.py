daca5e469e3c4b64d3b476e12bf76bc913e7db41
deanitasekar <deanitasekar@gmail.com>
2024-10-08 17:34:27 +0700

    AJAX implementation

--- main/views.py
+++ main/views.py
@@ -12,15 +12,14 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
 
 @login_required(login_url='/login')
 def show_main(request):
-    products = Product.objects.all()
     context = {
         'npm' : '2306229405',
         'name': request.user.username,
         'class': 'PBP D',
-        'products' : products,
         'last_login': request.COOKIES['last_login'],
     }
 
@@ -30,20 +29,20 @@ def create_product(request):
     form = ProductEntryForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        mood_entry = form.save(commit=False)
-        mood_entry.user = request.user
-        mood_entry.save()
+        product_entry = form.save(commit=False)
+        product_entry.user = request.user
+        product_entry.save()
         return redirect('main:show_main')
 
     context = {'form': form}
     return render(request, "create_product.html", context)
 
 def show_xml(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
@@ -94,7 +93,7 @@ def edit_product (request, id):
     # Get product berdasarkan id
     product = Product.objects.get(pk = id)
     
-    # Set product sebagai isntance dari form
+    # Set product sebagai instance dari form
     form = ProductEntryForm(request.POST or None, instance = product)
     
     if form.is_valid() and request.method == "POST":
@@ -114,4 +113,23 @@ def delete_product(request, id):
     
     # Kembali ke halaman awal
     return HttpResponseRedirect(reverse('main:show_main'))
+
+@csrf_exempt
+@require_POST
+def add_product_ajax(request):
+    name = strip_tags(request.POST.get("name"))
+    price = request.POST.get("price")
+    description = strip_tags(request.POST.get("description"))
+    stock = request.POST.get("stock")
+    user = request.user
+
+    new_product = Product(
+        name=name, price=price,
+        description=description, stock=stock,
+        user=user
+    )
+    new_product.save()
+
+    return HttpResponse(b"CREATED", status=201)
+    ...
     
